8a9df5d00602383235a3455545beddb890049d2b
Meysam Azari <meysam.azari79@gmail.com>
2023-06-02 15:08:55 +0330

    Projects page added and feature project completed

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -29,7 +29,7 @@ const singleword = {
 const AnimatedText = ({text , classname=""}) => {
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow=hidden'>
-        <motion.h1 className={`inline-block w-full text-dark font bold capitalize text-8xl ${classname}`}
+        <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${classname}`}
         variants={quote}
         initial="initial"
         animate="animate"
