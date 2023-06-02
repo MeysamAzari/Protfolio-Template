7331d98b0997a0994bea3f9569213eadb503a27c
Meysam Azari <meysam.azari79@gmail.com>
2023-06-02 22:05:16 +0330

    responsive to hireme page

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -28,7 +28,7 @@ const singleword = {
 }
 const AnimatedText = ({text , classname=""}) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow=hidden'>
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow=hidden sm:py-0'>
         <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${classname} dark:text-light`}
         variants={quote}
         initial="initial"
--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -3,7 +3,7 @@ import {CircularText} from './Icons'
 import Link from 'next/link'
 const HireMe = () => {
   return (
-    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
+    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto'>
         <div className='w-48 h-auto flex items-center justify-center relative'>
             <CircularText className={"fill-dark animate-spin-slow dark:fill-light"}/>
             <Link href="mailto:meysam.azari79@gmail.com" className='
